e92268a995cc500615a68ffd64ba931494c060df
yanhua.chen <yanhua.chen@bkjk.com>
2018-11-06 17:37:16 +0800

    增加一个不被增强的Bean，来调试和证明：Spring容器在启动时会尝试为容器中的每一个Bean创建代理，有代理就是代理后的Bean，没有就是普通Bean

---
+++ chapter07_aop/src/main/java/com/cyh2/NonAopBeanTest.java
@@ -0,0 +1,14 @@
+package com.cyh2;
+
+/**
+ * @author: yanhua.chen
+ * @date: 2018/11/6 17:28
+ */
+public class NonAopBeanTest {
+
+    public void thisIsNonAopBeanMethod() {
+        System.err.println("thisIsNonAopBeanMethod...");
+    }
+
+
+}
--- chapter07_aop/src/test/java/com/cyh/AopTest.java
+++ chapter07_aop/src/test/java/com/cyh/AopTest.java
@@ -2,11 +2,16 @@ package com.cyh;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import com.cyh2.NonAopBeanTest;
+
 public class AopTest {
 
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("aop-context.xml");
         context.getBean(TestBean.class).test();
+        System.err.println("==========================================================================");
+        context.getBean(NonAopBeanTest.class).thisIsNonAopBeanMethod();
+        System.err.println("==========================================================================");
     }
 
 }
